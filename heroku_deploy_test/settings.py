3015e4cd01d245dbcb4c4eeb37b9de8c5e5c79eb
heroku <heroku@heroku.com>
2022-01-27 22:21:56 +0530

    heroku setup

--- heroku_deploy_test/settings.py
+++ heroku_deploy_test/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+import django_heroku
 
 from pathlib import Path
 
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4+i+p3by4qy$oz4i-jb-5=e8d$%r1f^4!+mcl-r-lq_37#edet'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://calm-beyond-28808.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Activate Django-Heroku.
+django_heroku.settings(locals())
